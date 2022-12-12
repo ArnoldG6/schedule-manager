7656b57d3bd5edd520d6d1e67010846c9e5b8dc8
ArnoldG6 <arnoldgq612@gmail.com>
2022-12-12 10:49:46 -0600

    10.49pm

--- src/main/java/org/una/custom_fx_components/Draggable.java
+++ src/main/java/org/una/custom_fx_components/Draggable.java
@@ -9,82 +9,30 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Generalised implementation of 'Draggability' of a {@link Node}. The Draggable class is used as a 'namespace' for the internal
- * class/interfaces/enum.
- * @author phill
- *
+ * Modified version of @author phill
  */
 public class Draggable {
     public enum Event {
         None, DragStart, Drag, DragEnd
     }
 
-    /**
-     * Marker for an entity that has draggable nature.
-     * @author phill
-     */
-    public interface Interface {
-        public abstract Draggable.Nature getDraggableNature();
-    }
-
     public interface Listener {
         public void accept(Nature draggableNature, Event dragEvent);
     }
-
-    /**
-     * Class that encapsulates the draggable nature of a node.
-     * <ul>
-     * <li>EventNode: the event that receives the drag events</li>
-     * <li>One or more DragNodes: that move in response to the drag events. The EventNode is usually (but not always) a
-     * DragNode</li>
-     * <li>Listeners: listen for the drag events</li>
-     * </ul>
-     * @author phill
-     *
-     */
     public static final class Nature implements EventHandler<MouseEvent> {
         private double lastMouseX = 0, lastMouseY = 0; // scene coords
-
         private boolean dragging = false;
-
         private final boolean enabled = true;
         private final Node eventNode;
-        private final List<Node> dragNodes = new ArrayList<>();
-        private final List<Listener> dragListeners = new ArrayList<>();
-
+        private final Node dragNodes;
         public Nature(final Node node) {
             this(node, node);
         }
-
-        public Nature(final Node eventNode, final Node... dragNodes) {
+        public Nature(final Node eventNode, final Node dragNodes) {
             this.eventNode = eventNode;
-            this.dragNodes.addAll(Arrays.asList(dragNodes));
+            this.dragNodes = dragNodes;
             this.eventNode.addEventHandler(MouseEvent.ANY, this);
         }
-
-        public final boolean addDraggedNode(final Node node) {
-            if (!this.dragNodes.contains(node)) {
-                return this.dragNodes.add(node);
-            }
-            return false;
-        }
-
-        public final boolean addListener(final Listener listener) {
-            return this.dragListeners.add(listener);
-        }
-
-        public final void detatch() {
-            this.eventNode.removeEventFilter(MouseEvent.ANY, this);
-        }
-
-        public final List<Node> getDragNodes() {
-            return new ArrayList<>(this.dragNodes);
-        }
-
-        public final Node getEventNode() {
-            return this.eventNode;
-        }
-
         @Override
         public final void handle(final MouseEvent event) {
             if (MouseEvent.MOUSE_PRESSED == event.getEventType()) {
@@ -96,59 +44,30 @@ public class Draggable {
             } else if (MouseEvent.MOUSE_DRAGGED == event.getEventType()) {
                 if (!this.dragging) {
                     this.dragging = true;
-                    for (final Listener listener : this.dragListeners) {
-                        listener.accept(this, Draggable.Event.DragStart);
-                    }
                 }
                 if (this.dragging) {
                     final double deltaX = event.getSceneX() - this.lastMouseX;
                     final double deltaY = event.getSceneY() - this.lastMouseY;
-                    for (final Node dragNode : this.dragNodes) {
-                        final double initialTranslateX = dragNode.getTranslateX();
-                        final double initialTranslateY = dragNode.getTranslateY();
-                        dragNode.setTranslateX(initialTranslateX + deltaX);
-                        dragNode.setTranslateY(initialTranslateY + deltaY);
-                        System.out.println(dragNode.getTranslateX());
-                        System.out.println(dragNode.getTranslateY());
-                    }
+                    final double initialTranslateX = dragNodes.getTranslateX();
+                    final double initialTranslateY = dragNodes.getTranslateY();
+                    dragNodes.setTranslateX(initialTranslateX + deltaX);
+                    dragNodes.setTranslateY(initialTranslateY + deltaY);
+                    System.out.println(dragNodes.getTranslateX());
+                    System.out.println(dragNodes.getTranslateY());
                     this.lastMouseX = event.getSceneX();
                     this.lastMouseY = event.getSceneY();
-
                     event.consume();
-                    for (final Listener listener : this.dragListeners) {
-                        listener.accept(this, Draggable.Event.Drag);
-                    }
                 }
             } else if (MouseEvent.MOUSE_RELEASED == event.getEventType()) {
                 if (this.dragging) {
                     event.consume();
                     this.dragging = false;
-                    for (final Listener listener : this.dragListeners) {
-                        listener.accept(this, Draggable.Event.DragEnd);
-                    }
                     //System.out.println(lastMouseX);
                     //System.out.println(lastMouseY);
                 }
             }
-
         }
 
-        public final boolean removeDraggedNode(final Node node) {
-            return this.dragNodes.remove(node);
-        }
 
-        public final boolean removeListener(final Listener listener) {
-            return this.dragListeners.remove(listener);
-        }
-
-        /**
-         * When the initial mousePressed is missing we can supply the first coordinates programmatically.
-         * @param lastMouseX
-         * @param lastMouseY
-         */
-        public final void setLastMouse(final double lastMouseX, final double lastMouseY) {
-            this.lastMouseX = lastMouseX;
-            this.lastMouseY = lastMouseY;
-        }
     }
 }
